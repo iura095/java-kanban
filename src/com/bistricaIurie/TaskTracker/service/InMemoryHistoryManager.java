38395c71b9539798b214ddcaa5493a2ea4c8a884
bistrica.iurie <bistrica.iurie95@gmail.com>
2025-08-06 00:18:04 +0300

    3ый commit 6го спринта

--- src/com/bistricaIurie/TaskTracker/service/InMemoryHistoryManager.java
+++ src/com/bistricaIurie/TaskTracker/service/InMemoryHistoryManager.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
-public class InMemoryHistoryManager implements HistoryManager{
+public class InMemoryHistoryManager implements HistoryManager {
 
     private HashMap<Integer, Node> historyList = new HashMap<>();
     private Node head = new Node(null);
