b4771b0dbacc845617b0602712075b4c359f3aed
bistrica.iurie <bistrica.iurie95@gmail.com>
2025-08-20 16:09:04 +0300

    1ый соммит 7-го спринта

--- src/com/bistricaIurie/TaskTracker/model/FileCeckException.java
+++ src/com/bistricaIurie/TaskTracker/model/FileCeckException.java
@@ -5,6 +5,7 @@ public class FileCeckException extends Exception {
     public FileCeckException() {
       super();
     }
+
     public FileCeckException(String message) {
         super(message);
     }
