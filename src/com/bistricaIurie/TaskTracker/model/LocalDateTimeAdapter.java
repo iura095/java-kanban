2f03a0dc68126d257bf2d5f5ec8ea4eade40b3bf
bistrica.iurie <bistrica.iurie95@gmail.com>
2025-09-14 12:05:52 +0300

    1ый соммит 9-го спринта

--- src/com/bistricaIurie/TaskTracker/model/LocalDateTimeAdapter.java
+++ src/com/bistricaIurie/TaskTracker/model/LocalDateTimeAdapter.java
@@ -2,7 +2,6 @@ package com.bistricaIurie.TaskTracker.model;
 
 import com.google.gson.TypeAdapter;
 import com.google.gson.stream.JsonReader;
-import com.google.gson.stream.JsonToken;
 import com.google.gson.stream.JsonWriter;
 
 import java.io.IOException;
